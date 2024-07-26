704eee65a02b807fb95d39e6225a33251fe38316
Carlos-Intecap <carlostavico2024@gmail.com>
2024-07-26 11:52:58 -0600

    Diseno mejorado

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,46 +4,63 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Telefonía</title>
+    <title>Hoteles</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
-<body class="bg-body-secondary">
+<style>
+body {
+  background-image: url('https://imageio.forbes.com/specials-images/imageserve/5cdb058a5218470008b0b00f/Nobu-Ryokan-Malibu/0x0.jpg?format=jpg&height=1009&width=2000');
+}
+.card {
+    background-color: rgba(255, 99, 71, 0.3);
+}
+h1 {
+    color: white;
+    margin: 100px 0;
+}
+.card-body {
+    font-size: 200px;
+    color: white;
+}
+.card-body:hover {
+    color: rgb(255, 203, 167);
+}
+</style>
 
-    <div class="container">
-        <div class="row justify-content-center m-4">
-            <div class="col col-4 text-center">
-                <h1>Hoteles</h1>
-                <div class="list-group">
-                    <a href="clientes" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-person-fill fs-1"></i>
-                                <h5 class="card-title">Clientes</h5>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="hoteles" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-building-fill fs-1"></i>
-                                <h5 class="card-title">Hoteles</h5>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="reservaciones" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-card-checklist fs-1"></i>
-                                <h5 class="card-title">Reservaciones</h5>
-                            </div>
-                        </div>
-                    </a>
+<body class="bg-body-secondary">
+    <div class="container text-center">
+    <h1>Hoteles</h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+            <a href="clientes" class="col text-decoration-none">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Clientes</h5>
+                        <i class="bi bi-person-fill titulo"></i>
+                    </div>
                 </div>
-            </div>
+            </a>
+            <a href="hoteles" class="col  text-decoration-none">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Hoteles</h5>
+                        <i class="bi bi-building-fill"></i>
+                    </div>
+                </div>
+            </a>
+            <a href="reservaciones" class="col text-decoration-none">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Reservaciones</h5>
+                        <i class="bi bi-card-checklist"></i>
+                    </div>
+                </div>
+            </a>
         </div>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
